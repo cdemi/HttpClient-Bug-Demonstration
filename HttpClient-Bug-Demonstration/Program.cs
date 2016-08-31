d09cab5e87bc3f8a87b0ade9bdfa711c58a011f7
Christopher Demicoli <christopher.demicoli@outlook.com>
2016-08-31 23:22:32 +0200

    Initial project structure

---
+++ HttpClient-Bug-Demonstration/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpClient_Bug_Demonstration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
